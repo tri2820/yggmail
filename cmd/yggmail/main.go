c5666aa2aa14b6d861d42d2e2e6df2b271386c26
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2022-05-04 21:32:02 +1000

    ! work concurrent

--- cmd/yggmail/main.go
+++ cmd/yggmail/main.go
@@ -9,29 +9,24 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"crypto/ed25519"
 	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
+	"time"
 
-	"github.com/emersion/go-sasl"
-	"github.com/emersion/go-smtp"
+	// "github.com/fatih/color"
+
+	// "github.com/neilalexander/yggmail/internal/config"
+
+	// "github.com/neilalexander/yggmail/internal/smtpsender"
 	"github.com/fatih/color"
-	"golang.org/x/term"
-
-	"github.com/neilalexander/yggmail/internal/config"
-	"github.com/neilalexander/yggmail/internal/imapserver"
-	"github.com/neilalexander/yggmail/internal/smtpsender"
-	"github.com/neilalexander/yggmail/internal/smtpserver"
-	"github.com/neilalexander/yggmail/internal/storage/sqlite3"
 	"github.com/neilalexander/yggmail/internal/transport"
-	"github.com/neilalexander/yggmail/internal/utils"
 )
 
 type peerAddrList []string
@@ -45,17 +40,57 @@ func (i *peerAddrList) Set(value string) error {
 	return nil
 }
 
+func server(c net.Conn) {
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+
+	err := c.(*net.TCPConn).SetKeepAlive(true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = c.(*net.TCPConn).SetKeepAlivePeriod(5 * time.Second)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		// Receive
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf(">> %s", netData)
+		// Send
+		result := time.Now().String() + "\n"
+		c.Write([]byte(string(result)))
+	}
+}
+
+func client(c net.Conn) {
+	for {
+		// Send
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(c, text+"\n")
+		// Receive
+		message, _ := bufio.NewReader(c).ReadString('\n')
+		fmt.Printf("<< %s", message)
+	}
+}
+
 func main() {
 	rawlog := log.New(color.Output, "", 0)
 	green := color.New(color.FgGreen).SprintfFunc()
 	log := log.New(rawlog.Writer(), fmt.Sprintf("[  %s  ] ", green("Yggmail")), log.LstdFlags|log.Lmsgprefix)
 
 	var peerAddrs peerAddrList
-	database := flag.String("database", "yggmail.db", "SQLite database file")
-	smtpaddr := flag.String("smtp", "localhost:1025", "SMTP listen address")
-	imapaddr := flag.String("imap", "localhost:1143", "IMAP listen address")
+	secret := flag.String("sk", "", "IMAP listen address")
+	ping := flag.String("ping", "", "Send ping to address")
 	multicast := flag.Bool("multicast", false, "Connect to Yggdrasil peers on your LAN")
-	password := flag.Bool("password", false, "Set a new IMAP/SMTP password")
 	flag.Var(&peerAddrs, "peer", "Connect to a specific Yggdrasil static peer (this option can be given more than once)")
 	flag.Parse()
 
@@ -69,77 +104,27 @@ func main() {
 		os.Exit(0)
 	}
 
-	storage, err := sqlite3.NewSQLite3StorageStorage(*database)
-	if err != nil {
-		panic(err)
-	}
-	defer storage.Close()
-	log.Printf("Using database file %q\n", *database)
-
-	skStr, err := storage.ConfigGet("private_key")
-	if err != nil {
-		panic(err)
-	}
-
+	// "secret" -> sk -> pk
 	sk := make(ed25519.PrivateKey, ed25519.PrivateKeySize)
-	if skStr == "" {
-		if _, sk, err = ed25519.GenerateKey(nil); err != nil {
-			panic(err)
-		}
-		if err := storage.ConfigSet("private_key", hex.EncodeToString(sk)); err != nil {
-			panic(err)
-		}
-		log.Printf("Generated new server identity")
+	if *secret == "" {
+		_, sk, _ = ed25519.GenerateKey(nil)
 	} else {
-		skBytes, err := hex.DecodeString(skStr)
+		skBytes, err := hex.DecodeString(*secret)
 		if err != nil {
 			panic(err)
 		}
 		copy(sk, skBytes)
 	}
-	pk := sk.Public().(ed25519.PublicKey)
-	log.Printf("Mail address: %s@%s\n", hex.EncodeToString(pk), utils.Domain)
 
-	for _, name := range []string{"INBOX", "Outbox"} {
-		if err := storage.MailboxCreate(name); err != nil {
-			panic(err)
-		}
-	}
+	pk := sk.Public().(ed25519.PublicKey)
+	log.Printf("Yggdrasil address: %s\n", hex.EncodeToString(pk))
+	log.Printf("Secret key: %s\n", hex.EncodeToString(sk))
 
 	switch {
-	case password != nil && *password:
-		log.Println("Please enter your new password:")
-		password1, err := term.ReadPassword(int(os.Stdin.Fd()))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println()
-		log.Println("Please enter your new password again:")
-		password2, err := term.ReadPassword(int(os.Stdin.Fd()))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println()
-		if !bytes.Equal(password1, password2) {
-			log.Println("The supplied passwords do not match")
-			os.Exit(1)
-		}
-		if err := storage.ConfigSetPassword(strings.TrimSpace(string(password1))); err != nil {
-			log.Println("Failed to set password:", err)
-			os.Exit(1)
-		}
-
-		log.Println("Password for IMAP and SMTP has been updated!")
-		os.Exit(0)
 
 	case (multicast == nil || !*multicast) && len(peerAddrs) == 0:
 		log.Printf("You must specify either -peer, -multicast or both!")
 		os.Exit(0)
-	}
-
-	cfg := &config.Config{
-		PublicKey:  pk,
-		PrivateKey: sk,
 	}
 
 	transport, err := transport.NewYggdrasilTransport(rawlog, sk, pk, peerAddrs, *multicast)
@@ -147,73 +132,25 @@ func main() {
 		panic(err)
 	}
 
-	queues := smtpsender.NewQueues(cfg, log, transport, storage)
-	var notify *imapserver.IMAPNotify
-
-	imapBackend := &imapserver.Backend{
-		Log:     log,
-		Config:  cfg,
-		Storage: storage,
+	if *ping == "" {
+		// Server
+		l := transport.Listener()
+		defer l.Close()
+		for {
+			c, err := l.Accept()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			go server(c)
+		}
+	} else {
+		// Client
+		log.Println("Client code running")
+		c, err := transport.Dial(*ping)
+		if err != nil {
+			panic(err)
+		}
+		client(c)
 	}
-
-	_, notify, err = imapserver.NewIMAPServer(imapBackend, *imapaddr, true)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Listening for IMAP on:", *imapaddr)
-
-	go func() {
-		localBackend := &smtpserver.Backend{
-			Log:     log,
-			Mode:    smtpserver.BackendModeInternal,
-			Config:  cfg,
-			Storage: storage,
-			Queues:  queues,
-			Notify:  notify,
-		}
-
-		localServer := smtp.NewServer(localBackend)
-		localServer.Addr = *smtpaddr
-		localServer.Domain = hex.EncodeToString(pk)
-		localServer.MaxMessageBytes = 1024 * 1024
-		localServer.MaxRecipients = 50
-		localServer.AllowInsecureAuth = true
-		localServer.EnableAuth(sasl.Login, func(conn *smtp.Conn) sasl.Server {
-			return sasl.NewLoginServer(func(username, password string) error {
-				_, err := localBackend.Login(nil, username, password)
-				return err
-			})
-		})
-
-		log.Println("Listening for SMTP on:", localServer.Addr)
-		if err := localServer.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	go func() {
-		overlayBackend := &smtpserver.Backend{
-			Log:     log,
-			Mode:    smtpserver.BackendModeExternal,
-			Config:  cfg,
-			Storage: storage,
-			Queues:  queues,
-			Notify:  notify,
-		}
-
-		overlayServer := smtp.NewServer(overlayBackend)
-		overlayServer.Domain = hex.EncodeToString(pk)
-		overlayServer.MaxMessageBytes = 1024 * 1024
-		overlayServer.MaxRecipients = 50
-		overlayServer.AuthDisabled = true
-
-		if err := overlayServer.Serve(transport.Listener()); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs
-	log.Println("Shutting down")
 }
